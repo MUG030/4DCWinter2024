1f86e278769a67657af31b0ddf6c48af70519603
[MUG030] <ys8267928563@outlook.jp>
2024-02-09 06:36:02 +0900

    クリアScene完成

---
+++ Assets/Scripts/Manager/ClearCatalog.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearCatalog : MonoBehaviour
+{
+    [SerializeField] private GameObject beforeObject;
+    [SerializeField] private GameObject afterObject;
+
+    void Start()
+    {
+        beforeObject.SetActive(true);
+        afterObject.SetActive(false);
+
+        Invoke("ChangeCatalog", 10.0f);
+    }
+
+    private void ChangeCatalog()
+    {
+        beforeObject.SetActive(false);
+        afterObject.SetActive(true);
+    }
+}
