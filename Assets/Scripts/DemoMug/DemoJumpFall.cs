68a7c046798a7b4ed7071adbce06b0131d844aee
denchiddd <tuxedo0502@gmail.com>
2024-02-08 15:43:58 +0900

    上り下りの床を作成
    上キーでジャンプ、下キーで降りるが可能です。

---
+++ Assets/Scripts/DemoMug/DemoJumpFall.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoJumpFall : MonoBehaviour
+{
+
+    private float _Speed = 5.0f;
+    private float _JumpPower = 20.0f;
+    private Rigidbody2D _rb;
+   
+
+    public LayerMask whatIsGround;
+    private Transform groundCheckPoint;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        groundCheckPoint = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+
+        if ( inputY > 0 && _rb.velocity.y == 0)
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, _JumpPower);
+        }
+        else if( inputY < -0.5 && _rb.velocity.y == 0)
+        {
+            Debug.Log("‰º“ü—Í•Jump");
+            Collider2D[] hitFloors = Physics2D.OverlapCircleAll(groundCheckPoint.position, 2f, whatIsGround);
+            foreach(Collider2D hitFloor in hitFloors)
+            {
+                PlatformManager platformManager = hitFloor.GetComponent<PlatformManager>();
+                if ( platformManager != null )
+                {
+                    Debug.Log("ŒÄ‚Î‚ê‚½");
+                    hitFloor.GetComponent<PlatformManager>().Through();
+                }
+                
+            }
+                //= Physics2D.OverlapCircleAll(groundCheckPoint.position, 2f, whatIsGround);
+        }
+
+        Vector2 vector2 = new Vector2(inputX * _Speed, _rb.velocity.y);
+        _rb.velocity = vector2;
+    }
+    
+}
---
+++ Assets/Scripts/DemoMug/PlatformManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformManager : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    private PlatformEffector2D platformEffector2D;
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        platformEffector2D = GetComponent<PlatformEffector2D>();
+    }
+
+    public void Through()
+    {
+        platformEffector2D.surfaceArc = 0;
+        Invoke("Reset", 0.3f);
+    }
+
+    public void Reset()
+    {
+        platformEffector2D.surfaceArc = 180;
+    }
+}
