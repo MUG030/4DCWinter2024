ec593a8020ee34960703488ad0c339da573c4386
kokusanNasubi <deptiliaum44@gmail.com>
2024-02-09 09:18:38 +0900

    ステージ作成中

--- Assets/Scripts/StageTiles/TileGenerator.cs
+++ Assets/Scripts/StageTiles/TileGenerator.cs
@@ -13,11 +13,9 @@ public class TileGenerator : MonoBehaviour
     [SerializeField]
     public int tile_Ways_Amount = 1;
     private GameObject latestTile;
-    private TilesScroll latestTile_Scroll;
     void Start()
     {
         latestTile = tile_Departure;
-        latestTile_Scroll = latestTile.GetComponent<TilesScroll>();
         Test();
     }
     void Update()
@@ -32,15 +30,13 @@ public class TileGenerator : MonoBehaviour
             {
                 int i = Random.Range(0, tile_Ways.Count);
                 --tile_Ways_Amount;
-                latestTile = Instantiate(tile_Ways[i], new Vector3(j*69+17.5f, 0, 0), Quaternion.identity);
-                latestTile_Scroll = latestTile.GetComponent<TilesScroll>();
+                latestTile = Instantiate(tile_Ways[i], new Vector3(j*69+32f, 0, 0), Quaternion.identity);
                 Debug.Log(tile_Ways_Amount);
             }
             if(tile_Ways_Amount == 1)
             {
                 Debug.Log("Last");
-                latestTile = Instantiate(tile_Goal, new Vector3((j+1)*69+17.5f, 0, 0), Quaternion.identity);
-                latestTile_Scroll = latestTile.GetComponent<TilesScroll>();
+                latestTile = Instantiate(tile_Goal, new Vector3((j+1)*69+32f, 0, 0), Quaternion.identity);
             }
         }
 
